abb004dc22c00ccc8e9db02189a4e337aa9fd919
Mehedi Zaman <mehedizmn@gmail.com>
2017-07-09 23:57:32 +0600

    Commit with Travis CI integration

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,24 +1,29 @@
-import express from 'express';
+/*import express from 'express';
 import path from 'path';
 import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
+*/
+
+const express = require('express');
+const path = require('path');
+const open = require('open');
 
 /* eslint-disable no-console */
 
 const port = 3000;
 const app = express();
-const compiler = webpack(config);
+//const compiler = webpack(config);
 
-app.use(require('webpack-dev-middleware')(compiler, {
+/*app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
-}));
+})); */
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
-
+/*
 app.get('/users', function(req, res) {
   // Hard coding for simplicity. Pretend this hits a real database
   res.json([
@@ -27,7 +32,7 @@ app.get('/users', function(req, res) {
     {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
   ]);
 });
-
+*/
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
--- src/index.test.js
+++ src/index.test.js
@@ -19,3 +19,4 @@ describe('index.html', () => {
     });
   })
 })
+
